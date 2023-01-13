8de9e25d1382043fc76c941c70500494fa382bb0
joseph7Cartaphilus <artem.harov.120@mail.ru>
2023-01-13 14:56:11 +0300

    + list view and detail view

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movie_app/movies.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
 
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movie_app/movie_detail.html', {'movie': movie})
+    model = Movie
+    slug_field = 'url'
