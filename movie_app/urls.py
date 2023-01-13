31e71e2b7b0fe2cff3db677f6d218d36797f83a5
joseph7Cartaphilus <artem.harov.120@mail.ru>
2023-01-13 17:28:40 +0300

    + forms reviews

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -17,6 +17,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.MoviesView.as_view()),
-    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail')
+    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
+    path('reviews/<int:pk>/', views.AddReview.as_view(), name='add_review')
 ]
 
