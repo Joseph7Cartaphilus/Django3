92b3d516fb42b9dd397f595d058aaa19ea479b12
joseph7Cartaphilus <artem.harov.120@mail.ru>
2023-01-14 13:19:10 +0300

    + ajax

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 from .models import Movie, Category, Actor, Genre
@@ -60,3 +61,18 @@ class FilterMoviesView(GenreView, ListView):
             Q(genres__in=self.request.GET.getlist("genre"))
         )
         return queryset
+
+
+class JsonFilterMoviesView(ListView):
+    """Фильтр фильмов в json"""
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        ).distinct().values("title", "tagline", "url", "poster")
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset = list(self.get_queryset())
+        return JsonResponse({"movies": queryset}, safe=False)
