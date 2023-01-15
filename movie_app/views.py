de6db86aead6658be70cebdfa69f88d59baf96a4
joseph7Cartaphilus <artem.harov.120@mail.ru>
2023-01-15 13:50:12 +0300

    + add pagination

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Q
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.http import JsonResponse, HttpResponse
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie, Category, Actor, Genre, Rating
+from .models import Movie, Category, Actor, Genre, Rating, Reviews
 from .forms import ReviewsForm, RatingForm
 
 
@@ -14,19 +14,21 @@ class GenreView:
         return Genre.objects.all()
 
     def get_years(self):
-        return Movie.objects.filter(draft=False).values("year")
+        return Movie.objects.filter(draft=False).values("year").distinct()
 
 
 class MoviesView(GenreView, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
+    paginate_by = 3
 
 
 class MovieDetailView(GenreView, DetailView):
     """Полное описание фильма"""
 
     model = Movie
+    queryset = Movie.objects.filter(draft=False)
     slug_field = 'url'
 
     def get_context_data(self, **kwargs):
@@ -59,13 +61,20 @@ class ActorView(GenreView, DetailView):
 
 class FilterMoviesView(GenreView, ListView):
     """Фильтр фильмов"""
+    paginate_by = 2
 
     def get_queryset(self):
         queryset = Movie.objects.filter(
             Q(year__in=self.request.GET.getlist("year")) |
             Q(genres__in=self.request.GET.getlist("genre"))
-        )
+        ).distinct()
         return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["year"] = ''.join([f"year={x}&" for x in self.request.GET.getlist("year")])
+        context["genre"] = ''.join([f"genre={x}&" for x in self.request.GET.getlist("genre")])
+        return context
 
 
 class JsonFilterMoviesView(ListView):
