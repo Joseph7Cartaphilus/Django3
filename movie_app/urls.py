a047fca426beeaf236b630ce83991c0f35ed4ee4
joseph7Cartaphilus <artem.harov.120@mail.ru>
2023-02-01 00:24:17 +0300

    final remark

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -17,7 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MoviesView.as_view()),
+    path("", views.MoviesView.as_view(), name="main_page"),
     path("filter/", views.FilterMoviesView.as_view(), name="filter"),
     path("search/", views.Search.as_view(), name="search"),
     path("add-rating/", views.AddStarRating.as_view(), name="add_rating"),
